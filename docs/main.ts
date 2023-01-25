fa4965203c2ce1e5081aa8897c779a51bc43a931
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-01-24 18:04:38 -0800

    Updates

--- docs/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import '@/style.css';
-import App from '@/App.vue';
-import EasterEggTrigger from "@/index";
-
-const app = createApp(App);
-
-app.use(EasterEggTrigger);
-app.mount("#app");
